141ef385b06db7e93de1b49df9546f4ffa0f0993
Itor Carlos <itor51carlos@gmail.com>
2026-02-28 19:34:57 -0300

    feat(api): configure [Program.c]s

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TesteTecnico.Application.Interfaces;
+using TesteTecnico.Application.Services;
+using TesteTecnico.Domain.Repositories;
+using TesteTecnico.Infrastructure.Persistence;
+using TesteTecnico.Infrastructure.Persistence.Repositories;
+using TesteTecnico.Infrastructure.Storage;
+using TesteTecnico.Presentation.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IStorageService, LocalStorageService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+var app = builder.Build();
+
+app.UseMiddleware<GlobalExceptionMiddleware>();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles();
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
